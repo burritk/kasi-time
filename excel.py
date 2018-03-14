b83f7e04251ded9c003a26b94c46414a4e9be641
Julián García <julian2996@gmail.com>
2018-03-14 02:52:07 -0600

    Update excel.py

--- excel.py
+++ excel.py
@@ -7,6 +7,7 @@ c = conn.cursor()
 wb  = Workbook()
 ws = wb.active
 for index, row in enumerate(c.execute('select * from lyrics')):
+  print row
   ws['A' + str(index)] = row
   
 wb.save('test.xlsx')
