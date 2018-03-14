c920627710de40539e805ed29fb38898da78fe8e
Julián García <julian2996@gmail.com>
2018-03-14 17:20:31 -0600

    Update excel.py

--- excel.py
+++ excel.py
@@ -10,12 +10,12 @@ ws = wb.active
 for index, row in enumerate(c.execute('select * from lyrics')):
   TITLE, LYRICS, singer, lyrics, composition, arrangement, category = row
   print TITLE
-  ws[get_column_letter(index) + str(1)] = TITLE
-  ws[get_column_letter(index) + str(2)] = LYRICS
-  ws[get_column_letter(index) + str(3)] = singer
-  ws[get_column_letter(index) + str(4)] = lyrics
-  ws[get_column_letter(index) + str(5)] = composition
-  ws[get_column_letter(index) + str(6)] = arrangement
-  ws[get_column_letter(index) + str(7)] = category
+  ws[get_column_letter(1) + str(index)] = TITLE
+  ws[get_column_letter(2) + str(index)] = LYRICS
+  ws[get_column_letter(3) + str(index)] = singer
+  ws[get_column_letter(4) + str(index)] = lyrics
+  ws[get_column_letter(5) + str(index)] = composition
+  ws[get_column_letter(6) + str(index)] = arrangement
+  ws[get_column_letter(7) + str(index)] = category
   
 wb.save('test2.xlsx')
