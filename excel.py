9d7c3e80684fa70409796946b2c044f337b0619a
Julián García <julian2996@gmail.com>
2018-03-14 17:18:16 -0600

    Update excel.py

--- excel.py
+++ excel.py
@@ -1,4 +1,4 @@
-from openpyxl import Workbook
+from openpyxl import Workbook, get_column_letter
 import sqlite3
 
 conn = sqlite3.connect('kasi.db')
@@ -6,15 +6,15 @@ c = conn.cursor()
 
 wb  = Workbook()
 ws = wb.active
-for row in c.execute('select * from lyrics'):
+for index, row in enumerate(c.execute('select * from lyrics')):
   TITLE, LYRICS, singer, lyrics, composition, arrangement, category = row
   print TITLE
-  ws['A' + str(1)] = TITLE
-  ws['A' + str(2)] = LYRICS
-  ws['A' + str(3)] = singer
-  ws['A' + str(4)] = lyrics
-  ws['A' + str(5)] = composition
-  ws['A' + str(6)] = arrangement
-  ws['A' + str(7)] = category
+  ws[get_column_letter(index) + str(1)] = TITLE
+  ws[get_column_letter(index) + str(2)] = LYRICS
+  ws[get_column_letter(index) + str(3)] = singer
+  ws[get_column_letter(index) + str(4)] = lyrics
+  ws[get_column_letter(index) + str(5)] = composition
+  ws[get_column_letter(index) + str(6)] = arrangement
+  ws[get_column_letter(index) + str(7)] = category
   
-wb.save('test.xlsx')
+wb.save('test2.xlsx')
